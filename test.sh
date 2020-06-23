772a9b7c3817128c8f4f7253caa3b5630e9524f8
cu2 <nemeth.andras.mail@gmail.com>
2020-06-23 13:16:21 +0200

    test.sh and tsconfig updated; a bit hacky so far

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -eEuo pipefail
 
-
 # start server
-PORT=9876 npm run start &
+# PORT=9876 npm run start &
+PORT=9876 node ./dist/index.js &
 PID=$!
 
 # stop server at end or interrupt
