c0c1f2c20fc180d159fc0a7410831a912291c0b3
Magyar Máté <dev@mmagyar.com>
2020-06-26 09:27:43 +0000

    fix search

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import { info } from 'declarapi'
 import { registerRestMethods } from 'declarapi/dist/runtime/registerRestMethods'
 import { addValidationToContract } from 'declarapi/dist/runtime/contractValidation'
 import { contracts } from './generated-code/api-schema-server'
@@ -37,10 +38,30 @@ app.get('/health', (req, res) => {
 const contractsWithValidation = addValidationToContract(contracts)
 const restMethods = registerRestMethods(contractsWithValidation)
 restMethods.forEach((expressable) => {
-  app[expressable.method](expressable.route, (req, res) => expressable.handler(req, res))
+  app[expressable.method](expressable.route,
+    (req, res) =>
+      expressable.handler(req, res))
 })
 
+app.get('/elastic', async (_, res) => {
+  res.json({ info: await info().catch(console.warn) })
+})
+
+app.route('/api/*').all((_, res) => {
+  res.status(404)
+  res.json({ status: 404, error: 'not found', info: 'no method implemented under this url' })
+})
+
+const missingApi = (_:any, res:any) => {
+  res.status(405)
+  res.json({ status: 405, error: 'method not allowed', info: 'All API calls must be under /api' })
+}
+
+app.post('*', missingApi)
+app.put('*', missingApi)
+app.patch('*', missingApi)
+app.delete('*', missingApi)
+
 app.listen(port, () => {
-  // tslint:disable-next-line:no-console
   console.log(`Server started at http://localhost:${port}`)
 })
